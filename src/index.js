f600916a5eaafa51b70d22017409bd0e319a5d4b
olci34 <developerogulcan@gmail.com>
2023-08-15 01:13:28 -0400

    Bring Three.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createRoot } from 'react-dom/client';
+import './App.css';
+import { App } from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const pexel = (id) =>
+  `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`;
+const images = [
+  // Front
+  {
+    position: [0, 0, 1.5],
+    rotation: [0, 0, 0],
+    url: pexel(1103970),
+    text: 'Welcome\nThis is my website',
+  },
+  // Back
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(416430), text: 'Text 1' },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: pexel(310452), text: 'Text 2' },
+  // Left
+  {
+    position: [-1.75, 0, 0.25],
+    rotation: [0, Math.PI / 2.5, 0],
+    url: pexel(327482),
+    text: 'Text 3',
+  },
+  {
+    position: [-2.15, 0, 1.5],
+    rotation: [0, Math.PI / 2.5, 0],
+    url: pexel(325185),
+    text: 'Text 4',
+  },
+  // { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: pexel(358574), text: 'Text 5' },
+  // Right
+  {
+    position: [1.75, 0, 0.25],
+    rotation: [0, -Math.PI / 2.5, 0],
+    url: pexel(227675),
+    text: 'Text 6',
+  },
+  {
+    position: [2.15, 0, 1.5],
+    rotation: [0, -Math.PI / 2.5, 0],
+    url: pexel(911738),
+    text: 'Text 7',
+  },
+  // { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: pexel(1738986), text: 'Text 8' },
+];
+
+createRoot(document.getElementById('root')).render(<App images={images} />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
